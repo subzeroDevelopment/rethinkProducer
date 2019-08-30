e0c47ea48b2e45259eb72d84f8693d851751446f
Oribe Abad <oribe@insaite.io>
2019-08-29 20:38:37 -0500

    add fisrt testing insert

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/subzeroDevelopment/rethinkProducer
 
 go 1.12
+
+require gopkg.in/rethinkdb/rethinkdb-go.v5 v5.0.1
--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+
+	r "gopkg.in/rethinkdb/rethinkdb-go.v5"
+)
+
+func main() {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "localhost:32769",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = r.DB("nitifications").Table("message").Insert(map[string]string{
+		"msg":  "Hellow world from go",
+		"name": "Oribe",
+	},
+	).Exec(session)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
